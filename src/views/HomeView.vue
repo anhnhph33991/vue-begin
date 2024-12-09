80d8f996a0cb55c963aa8d62cc504cd9fa29d819
anhnhph33991 <hoang838604@gmail.com>
2024-12-09 23:10:32 +0700

    feat: module_1 - syntax basic vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,83 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <button @click="removeCounter" class="btn">-</button>
+    <span class="counter">{{ counter }}</span>
+    <button @click="increaseCounter" class="btn">+</button>
   </div>
 </template>
 
-<!-- <main>
-  <TheWelcome />
-</main> -->
+<script setup>
+import { ref } from "vue";
+
+const counter = ref(0);
+
+const increaseCounter = () => {
+  counter.value++;
+};
+
+const removeCounter = () => {
+  counter.value--;
+};
+</script>
+
+<!-- ########  -->
+<!-- <script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const counter = ref(0);
+
+    const increaseCounter = () => {
+      counter.value++;
+    };
+
+    const removeCounter = () => {
+      counter.value--;
+    };
+
+    return {
+      counter,
+      increaseCounter,
+      removeCounter,
+    };
+  },
+};
+</script> -->
+
+<!-- ########  -->
+<!-- <script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++;
+    },
+    removeCounter() {
+      if (this.counter == 1) {
+        alert("Bạn không thể trừ nữa");
+        return;
+      }
+
+      this.counter--;
+    },
+  },
+};
+</script> -->
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
