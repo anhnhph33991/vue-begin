eccb336d82a6bb59d5a6bd2bac457346b5330c1f
anhnhph33991 <hoang838604@gmail.com>
2024-12-17 01:28:17 +0700

    useRoute

--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -2,20 +2,29 @@
   <div>
     <h1>Detail Post: {{ $route.params.id }}</h1>
 
+    <div>
+      <button @click="showPostId">Show Post Id</button>
+    </div>
+
     <router-link to="/posts">Back Posts</router-link>
   </div>
 </template>
 
 <script setup>
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
+const router = useRouter();
 
 const name = route.query.name;
 const price = route.query.price;
 
+const showPostId = () => {
+  alert(`The id of this post is: ${route.params.id}`);
+};
+
 // console.log(route.params.id);
-console.log(route);
+// console.log(router);
 
 // if (name && price) {
 //   console.log(`Name: ${name} --- Price: ${price}`);
