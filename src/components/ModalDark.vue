a80d523a30a967e14bad14920fdd0a79a71d157f
anhnhph33991 <hoang838604@gmail.com>
2024-12-17 15:28:52 +0700

    Dynamic components

---
+++ src/components/ModalDark.vue
@@ -0,0 +1,66 @@
+<template>
+    <!-- to theo selector body là tag / # -> id / . -> class 
+            dịch chuyển 1 div lên 1 div khác 
+        -->
+    <Teleport to="body">
+        <div class="modal-dark" v-if="modelValue">
+            <h1>
+                {{ title }}
+            </h1>
+
+            <slot />
+
+            <!-- <button @click="$emit('hideModal')">Hide Modal</button> -->
+            <button @click="handleButtonClick">Hide Modal</button>
+        </div>
+    </Teleport>
+</template>
+
+<script setup>
+// import { useSlots } from 'vue';
+
+// const slots = useSlots();
+
+// console.log(slots.title());
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: "No title huhu"
+    },
+    modelValue: {
+        type: Boolean,
+        default: false
+    }
+})
+
+// const props = defineProps(['title'])
+
+
+// console.log(props.title);
+
+/**  Emits */
+
+const emit = defineEmits(['update:modelValue'])
+
+const handleButtonClick = () => {
+    // emit('hideModal')
+
+    emit('update:modelValue', false)
+}
+
+</script>
+
+<style scoped>
+.modal-dark {
+    background: #333;
+    color: white;
+    padding: 10px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,16 +2,23 @@
     <div class="modals">
         <h1>ModalsView</h1>
 
+        <div>
+            <label>Show dark modals</label>
+            <input type="checkbox" v-model="showDarkModals">
+        </div>
+
+        <pre>{{ showDarkModals }}</pre>
+
         <button @click="showModal = !showModal">Toggle modal</button>
 
-        <Modal v-model="showModal" title="My modal title (vie props)">
+        <component :is="showDarkModals ? ModalDark : Modal" v-model="showModal" title="My modal title (vie props)">
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit, ratione consequatur porro veritatis
                 eligendi odio assumenda nemo iusto quam, ea possimus excepturi accusamus temporibus suscipit
                 impedit,
                 qui perferendis accusantium. Doloremque?</p>
 
             <h1>Hello</h1>
-        </Modal>
+        </component>
 
     </div>
 </template>
@@ -19,6 +26,8 @@
 <script setup>
 import { ref } from 'vue';
 import Modal from '@/components/Modal.vue';
+import ModalDark from '@/components/ModalDark.vue';
 
+const showDarkModals = ref(false)
 const showModal = ref(false)
 </script>
